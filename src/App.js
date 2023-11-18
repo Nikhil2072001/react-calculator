7e2220dddcf9a2a97c566b9366acdaf74c5e62f3
Nikhil77sutara <nikhilchoudhary@sutara.org>
2023-11-18 16:33:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+const [no, setNo] = useState({first:"", scond:""});
+const [secondNo, setSecondNo] = useState("");
+const [success,setSuccess] = useState("");
+const [error,setError] = useState(false);
+let {first,second} = no;
+function handleChange(e){
+  e.preventDefault();
+  setNo({...no,[e.target.name]:e.target.value});
+  console.log(no);
+  console.log(">>>>><<<<<<"+typeof(no.first))
+}
+function addition(){
+  
+  if ( no.first >= Number.MIN_VALUE && no.second <=Number.MAX_VALUE) {
+    setSuccess(`Result : ${Math.floor(no.first)+Math.floor(no.second)}`);
+    setError(false);
+  } else {
+  setError(true);
+  setSuccess("");}
+  first="";
+  second="";
+}
+function substraction(){
+  if ( no.first >= Number.MIN_VALUE && no.second <=Number.MAX_VALUE) {
+    setSuccess(`Result : ${Math.floor(no.first)-Math.floor(no.second)}`);
+    setError(false);
+  } 
+  else {
+  setError(true);
+  setSuccess("");}
+  first="";
+  second="";
+}
+function multiplication(){
+  if ( no.first >= Number.MIN_VALUE && no.second <=Number.MAX_VALUE) {
+    setSuccess(`Result : ${Math.floor(no.first)*Math.floor(no.second)}`);
+    setError(false);
+  } 
+  else {
+  setError(true);
+  setSuccess("");}
+  first="";
+  second="";
+}
+function division(){
+  if ( no.first >= Number.MIN_VALUE && no.second <=Number.MAX_VALUE) {
+    setSuccess(`Result : ${Math.floor(no.first)/Math.floor(no.second)}`);
+    setError(false);
+  } 
+  else {
+  setError(true);
+  setSuccess("");}
+  first="";
+  second="";
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <input placeholder='Enter first number' name='first' value={first} onChange={handleChange}/>
+    <input placeholder='Enter second number' name='second' value={second} onChange={handleChange}/>
+    <button onClick={addition}>+</button>
+    <button onClick={substraction}>-</button>
+    <button onClick={multiplication}>*</button>
+    <button onClick={division}>/</button>
+    {
+      success && (
+        <div>
+          <h2 style={{color:"green"}}>Success!</h2>
+         <p>{success}</p>
+        </div>
+      )
+    }
+    {
+      error && (
+        <div>
+          <h2 style={{color:"red"}}>Error!</h2>
+         <p>First number cannot be empty</p>
+        </div>
+      )
+    }
+
+   </div>
   );
 }
 
